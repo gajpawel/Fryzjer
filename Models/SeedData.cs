eda1f271d53e3d6337dcd5073f49fdb8e98f00bf
gajpawel <pg305038@student.polsl.pl>
2024-11-13 22:06:21 +0100

    Baza
    Dodana tabela z klientami

---
+++ Models/SeedData.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Fryzjer.Data;
+
+namespace Fryzjer.Models
+{
+    public class SeedData
+    {
+        //tu można zainicjowac rekordy do tabeli Client
+        //można rzucać wyjątki
+    }
+}
