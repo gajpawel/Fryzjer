debec292856fe7ba0f41f8f2c23d6957cc96f50b
gajpawel <pg305038@student.polsl.pl>
2024-11-17 13:34:53 +0100

    Poprawki

--- Models/Hairdresser.cs
+++ Models/Hairdresser.cs
@@ -10,7 +10,7 @@ namespace Fryzjer.Models
         public string? Name { get; set; }
         public string? surname { get; set; }
         public string? login { get; set; } // zadbać by był niepowtarzalny
-        public string? password { get; set; } // nałożyć wymagania silengo hasła
+        public string? password { get; set; } // nałożyć wymagania silnego hasła
         public string? photoPath { get; set; } //ścieżka do zdjęcia profilowego
         public char status { get; set; } // A - pracuje, X - zwolniony, T - tymczasowo bez przypisanego lokalu
         
